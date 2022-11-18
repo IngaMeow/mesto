df50ea862566f90dc2da70aeb726a46481f183d1
Inga Sevkovsaya <sevkvskaja@icloud.com>
2022-11-18 15:41:55 +0300

    fix: edit html/css/js after review

--- scripts/index.js
+++ scripts/index.js
@@ -2,35 +2,32 @@ const popupElement = document.querySelector ('.popup');
 const popupOpenButtonElement = document.querySelector ('.profile__edit-button');
 const popupCloseButtonElement = popupElement.querySelector ('.popup__close-button');
 
+
 let formElement = popupElement.querySelector('.popup__form');
-let nameInput = popupElement.querySelector('.popup__text_name');
-let jobInput = popupElement.querySelector('.popup__text_description');
+let nameInput = popupElement.querySelector('.popup__form-input_name');
+let jobInput = popupElement.querySelector('.popup__form-input_description');
 let profileNameElement = document.querySelector('.profile__name');
 let profileAboutElement = document.querySelector('.profile__description');
- 
 
-const OpenPopup = function() {
+const openPopup = function() {
   popupElement.classList.add('popup_is-opened');
   nameInput.value = profileNameElement.textContent;
   jobInput.value = profileAboutElement.textContent;
 }
 
-const ClosePopup = function() {
+const closePopup = function() {
   popupElement.classList.remove('popup_is-opened');
 }
 
-OpenPopup();
-ClosePopup();
+popupOpenButtonElement.addEventListener('click', openPopup); 
+popupCloseButtonElement.addEventListener('click', closePopup); 
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   
   profileNameElement.textContent = nameInput.value;
   profileAboutElement.textContent = jobInput.value;
-
-  ClosePopup();
+  closePopup();
 }
 
-popupOpenButtonElement.addEventListener('click', OpenPopup); 
-popupCloseButtonElement.addEventListener('click', ClosePopup); 
 formElement.addEventListener('submit', formSubmitHandler);
